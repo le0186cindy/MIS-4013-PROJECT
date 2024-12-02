32be9a4d387f13a06737ec1422442d1c8c44122e
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-02 10:59:22 -0600

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -6,10 +6,10 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item">
-        <img src="https://m.media-amazon.com/images/S/aplus-media-library-service-media/ffc27e70-4214-4efd-b605-e4d815cefdb0.__CR0,0,650,350_PT0_SX650_V1___.png" class="d-block w-100" alt="pic1" width="1000" height="600" style="object-fit: cover;">
+        <img src="" class="d-block w-100" alt="pic1" width="1000" height="600" style="object-fit: cover;">
     </div>
     <div class="carousel-item active">
-      <img src="https://chumley.barstoolsports.com/union/2024/11/20/dc884-17262004042678.06fe99d7.webp?fit=bounds&format=pjpg&auto=webp&quality=85%2C75" class="d-block w-100" alt="pic2" width="1000" height="600" style="object-fit: cover;">
+      <img src="" class="d-block w-100" alt="pic2" width="1000" height="600" style="object-fit: cover;">
     </div>
     <div class="carousel-item">
       <img src="..." class="d-block w-100" alt="pic3" width="1000" height="600" style="object-fit: cover;">
