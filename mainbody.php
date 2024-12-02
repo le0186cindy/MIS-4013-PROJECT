c062f3cc767eead7fd14e46f133fe6f23532c7be
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-02 11:41:40 -0600

    Update mainbody.php

--- mainbody.php
+++ mainbody.php
@@ -4,9 +4,9 @@
 
 <div class="container text-center">
     <div class="row row-cols-2 align-center">
-        <div class="col my-2"><img src="https://i.pinimg.com/736x/de/c6/53/dec653e6a15a78d8f5d93070d9fb9e17.jpg" alt="img1" width="60%" height="40%" class="img-fluid"></div>
-        <div class="col my-2"><img src="https://i.pinimg.com/736x/b7/99/9e/b7999ef48b98b2fa5dc52b53f696a6f6.jpg" alt="img2" width="60%" height="40%" class="img-fluid"></div>
-        <div class="col my-2"><img src="" alt="img3" width="60%" height="40%" class="img-fluid"></div>
-        <div class="col my-2"><img src="" alt="img4" width="60%" height="40%" class="img-fluid"></div>
+        <div class="col my-2"><img src="https://i.pinimg.com/736x/de/c6/53/dec653e6a15a78d8f5d93070d9fb9e17.jpg" alt="img1" width="60%" height="40%" style="object-fit: cover;" class="img-fluid"></div>
+        <div class="col my-2"><img src="https://i.pinimg.com/736x/b7/99/9e/b7999ef48b98b2fa5dc52b53f696a6f6.jpg" alt="img2" width="60%" height="40%" style="object-fit: cover;" class="img-fluid"></div>
+        <div class="col my-2"><img src="" alt="img3" width="60%" height="40%" style="object-fit: cover;" class="img-fluid"></div>
+        <div class="col my-2"><img src="" alt="img4" width="60%" height="40%" style="object-fit: cover;" class="img-fluid"></div>
     </div>
 </div>
