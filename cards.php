73d5b706d549a524ac19f70ae61758f5ded66088
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-02 11:31:02 -0600

    Update cards.php

--- cards.php
+++ cards.php
@@ -1,6 +1,6 @@
 <div class="d-flex justify-content-center mx-5 my-4 column-gap-5">
     <div class="card" style="width: 30rem;">
-    <img src="https://i.pinimg.com/736x/5e/77/5a/5e775a1ee6555d7262174972ab503b20.jpg" width="10rem" height="10rem" class="card-img-top" alt="...">
+    <img src="https://i.pinimg.com/736x/5e/77/5a/5e775a1ee6555d7262174972ab503b20.jpg" height="20rem" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -9,7 +9,7 @@
     </div>
 
     <div class="card" style="width: 30rem;">
-    <img src="https://i.pinimg.com/736x/55/07/72/55077258cf3bb86c63828687af957c40.jpg" width="10rem" height="10rem" class="card-img-top" alt="...">
+    <img src="https://i.pinimg.com/736x/55/07/72/55077258cf3bb86c63828687af957c40.jpg" height=20rem" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -18,7 +18,7 @@
     </div>
 
     <div class="card" style="width: 30rem;">
-    <img src="https://i.pinimg.com/736x/e7/24/95/e724959c7ef135bf0d931b4e3771a8e6.jpg" width="10rem" height="10rem" class="card-img-top" alt="...">
+    <img src="https://i.pinimg.com/736x/e7/24/95/e724959c7ef135bf0d931b4e3771a8e6.jpg" height="20rem" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
