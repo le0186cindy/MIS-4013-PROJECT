be6bb060b4af75d778c6916ee0d48f191aa20286
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-04 11:50:09 -0600

    Update products_body.php

--- components/products_body.php
+++ components/products_body.php
@@ -13,7 +13,7 @@
             foreach ($products as $product) {
         ?>
             <div class="card" style="width: 18rem;">
-                <img src="https://images-ext-1.discordapp.net/external/yfhix5ZTVfQfrXB9NzfOda_RHnpD5J-bZJikrymWC1Y/https/i.pinimg.com/736x/e9/f4/fc/e9f4fc74c5805ae6afa9f567ef9ce0ce.jpg?format=webp&width=530&height=662" class="card-img-top" alt="..." width="200px" height="200px" style="object-fit: cover;">
+                <img src="<?php echo $product['product_img'] ?>" class="card-img-top" alt="..." width="200px" height="200px" style="object-fit: cover;">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $product['product_name'] ?></h5>
                     <p class="card-text"><?php echo $product['product_description'] ?></p>
