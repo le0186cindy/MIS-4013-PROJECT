593f802e84525f640a839350cc7de8aace932d8f
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-04 11:22:23 -0600

    Update products_body.php

--- components/products_body.php
+++ components/products_body.php
@@ -12,7 +12,14 @@
         <?php
             foreach ($products as $product) {
         ?>
-            <!-- product card here-->
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo $product['product_img']?>" class="card-img-top" alt="..." width="200px" height="200px" style="object-fit: cover;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $product['product_name'] ?></h5>
+                    <p class="card-text"><?php echo $product['product_description'] ?></p>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                </div>
+            </div>
         <?php
             }
         ?>
