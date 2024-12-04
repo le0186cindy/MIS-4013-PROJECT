130dde51ee11712f6b57160858ea09ec048cd3fe
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-04 11:47:19 -0600

    Update products_body.php

--- components/products_body.php
+++ components/products_body.php
@@ -8,12 +8,12 @@
     <div>
         <h4>Products</h4>
     </div>
-    <div class="d-flex">
+    <div class="d-flex gap-5">
         <?php
             foreach ($products as $product) {
         ?>
             <div class="card" style="width: 18rem;">
-                <img src="<?php echo $product['product_img']?>" class="card-img-top" alt="..." width="200px" height="200px" style="object-fit: cover;">
+                <img src="https://images-ext-1.discordapp.net/external/yfhix5ZTVfQfrXB9NzfOda_RHnpD5J-bZJikrymWC1Y/https/i.pinimg.com/736x/e9/f4/fc/e9f4fc74c5805ae6afa9f567ef9ce0ce.jpg?format=webp&width=530&height=662" class="card-img-top" alt="..." width="200px" height="200px" style="object-fit: cover;">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $product['product_name'] ?></h5>
                     <p class="card-text"><?php echo $product['product_description'] ?></p>
