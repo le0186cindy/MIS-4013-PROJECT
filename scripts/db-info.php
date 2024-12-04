26da48665053b7c28a6820d6369fd61c6a41d45a
le0186cindy <157073379+le0186cindy@users.noreply.github.com>
2024-12-04 11:39:09 -0600

    Update db-info.php

--- scripts/db-info.php
+++ scripts/db-info.php
@@ -1,9 +1,4 @@
 <?php
-    $host = '138.197.17.168';
-    $username = 'cindyleo_cindy';
-    $password = 'L00pyle245';
-    $dbname = 'cindyleo_project';
-
     function get_db_connection(){
         // Create connection
         $conn = new mysqli('138.197.17.168', 'cindyleo_cindy', 'L00pyle245', 'cindyleo_project');
